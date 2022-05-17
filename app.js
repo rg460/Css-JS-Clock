a4ba1ff84de638bda2d874333fa3451d07e9012a
ravigordon <rgordz460@outlook.com>
2022-05-17 14:32:34 +0100

    setDate function created

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const { timeout } = require("nodemon/lib/config");
 
 function setDate(){
-
+    const date = new Date();
+    const secondsDegress = (seconds/ 60) * 360;
+    const seconds = now.getSeconds();
+    console.log(seconds);
 }
 setInterval(setDate, timeout,1000);
