3cd10dfe92fef2db4e799aea5b007742fdabe896
ravigordon <rgordz460@outlook.com>
2022-05-17 14:40:31 +0100

    second hand bug fixed , now second hand is moving

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 const secondHand = document.querySelector('.second-hand');
 function setDate(){
-    const date = new Date();
-    const secondsDegress = (seconds/ 60) * 360;
+    const now = new Date();
     const seconds = now.getSeconds();
+    const secondsDegress = (seconds/ 60) * 360;
+    secondHand.style.transform = `rotate(${secondsDegress}deg)`
     console.log(seconds);
 }
-setInterval(setDate, timeout,1000);
+setInterval(setDate,1000);
