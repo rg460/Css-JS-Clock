c6d8027bcda83faf20d84e7b10aeb1983b050acb
ravigordon <rgordz460@outlook.com>
2022-05-17 14:46:36 +0100

    mins hand set up

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 const secondHand = document.querySelector('.second-hand');
+const minsHand = document.querySelector('.min-hand');
 function setDate(){
     const now = new Date();
     const seconds = now.getSeconds();
-    const secondsDegress = (seconds/ 60) * 360;
+    const secondsDegress = ((seconds/ 60) * 360) + 90;
     secondHand.style.transform = `rotate(${secondsDegress}deg)`
-    console.log(seconds);
+    
+    const mins  = now.getMinutes();
+    const minsDegress = ((mins/ 60) * 360) + 90;
+    min
 }
 setInterval(setDate,1000);
